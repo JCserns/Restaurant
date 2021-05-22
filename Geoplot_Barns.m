75ced4d3b1fad23234f840bc1944ca2b8d433429
Jack Csernica <84102294+JCserns@users.noreply.github.com>
2021-05-21 21:54:05 -0400

    Update Geoplot_Barns.m
    added comments

--- Geoplot_Barns.m
+++ Geoplot_Barns.m
@@ -1,5 +1,5 @@
-latBarn = 39.9765283;
-longBarn = -75.3360636;
+latBarn = 39.9765283; %latitude of Barnabys
+longBarn = -75.3360636; %longitude of Barnabys
 geoscatter([latBarn],[longBarn],'r*')
 geolimits([39.8 40],[-75.5 -75.1])
 geobasemap satellite
